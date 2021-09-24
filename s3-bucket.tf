91a72b661131e017579f623d00d38b97e309bd12
sushant mahalle <sushant.mahalle1@gmail.com>
2021-09-24 20:03:30 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,5 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/sushantmahalle-training/s3-bucket/aws"
   version = "2.2.0"
   # insert required variables here
-  bucket_prefix = "sushant"
+  bucket_prefix = "S!"
 }
